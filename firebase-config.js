4c6f64145a7cd629ec071cbceffaafb1fc107da8
Michael Shivers <shiversmichael1@gmail.com>
2024-12-30 19:59:06 -0500

    Updated main files for database functionality

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+/* File: firebase-config.js */
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQ3OP9KQ4BCR2Dsmt38_vu5OlK3hRDNoE",
+  authDomain: "supporterweb-firebase.firebaseapp.com",
+  databaseURL: "https://supporterweb-firebase-default-rtdb.firebaseio.com",
+  projectId: "supporterweb-firebase",
+  storageBucket: "supporterweb-firebase.appspot.com",
+  messagingSenderId: "503357203652",
+  appId: "1:503357203652:web:5a7ba3fc2daa27c758d591",
+  measurementId: "G-CFWKSHJ4W7"
+};
+
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const database = getDatabase(app);
---
+++ prayer-tracker.js
@@ -0,0 +1,38 @@
+/* File: prayer-tracker.js */
+import { database } from "./firebase-config.js";
+import { ref, push, onValue, update, remove } from "firebase/database";
+
+const prayerListRef = ref(database, 'prayerList');
+
+function renderPrayerList() {
+  onValue(prayerListRef, (snapshot) => {
+    const data = snapshot.val();
+    const table = document.getElementById('prayer-table');
+    table.innerHTML = '<tr><th>Person</th><th>Prayer Request</th><th>Note</th><th>Select</th></tr>';
+
+    for (const key in data) {
+      const row = table.insertRow();
+      row.innerHTML = `
+        <td>${data[key].name}</td>
+        <td>${data[key].prayer}</td>
+        <td>${data[key].note}</td>
+        <td><input type="checkbox" data-key="${key}"></td>
+      `;
+    }
+  });
+}
+
+function addPrayer() {
+  const name = document.getElementById('newName').value;
+  const prayer = document.getElementById('newDetail').value;
+  const note = document.getElementById('newNote').value;
+  const image = document.getElementById('newImage').files[0];
+
+  push(prayerListRef, { name, prayer, note, image: image ? image.name : '' });
+  document.getElementById('newName').value = '';
+  document.getElementById('newDetail').value = '';
+  document.getElementById('newNote').value = '';
+}
+
+document.getElementById('add-person').addEventListener('click', addPrayer);
+renderPrayerList();
